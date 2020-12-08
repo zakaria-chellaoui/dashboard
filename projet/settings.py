dd7fa2cca44c8f26ffcbdd3a9af2951aba60bf2f
zakaria-chellaoui <chellaoui.zakaria@gmail.com>
2020-12-08 01:19:49 +0100

    frentend_whitout_modification

--- projet/settings.py
+++ projet/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@3!vgb(jx%%hrrtyc9)m_%7otwy*vfi-&u-tf_u9fyvsvrs%we'
+SECRET_KEY = '4bmtuy@ry!2g7v+&0hk_cf9_+h92+++iqy43pc4#=^p14*4vcm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
